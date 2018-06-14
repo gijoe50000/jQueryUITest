524d711dd88122ca13da8c57ce8f794c8676c8b7
joeos <gijoe0000@gmail.com>
2018-06-14 13:15:06 +0100

    added jQueryUI slider form unfinished

--- content.js
+++ content.js
@@ -7,4 +7,24 @@ $(function() {
 
     // $('#head1').delay(1000).toggle('explode');
 
+var $range=$('#ranges');
+
+
+    $('#ranges').slider(
+        {
+            range:true,
+            min:0,
+            max:120,
+            values:[15,60],
+            slide: function(event, ui){  '$'+ui.value[0] + '-$' + ui.values[1]
+                 }
+        });
+
+        // $('#amount').val("10");
+$('#amount').val(
+    '€'+ $range.slider('values',0)+'-€'+ $range.slider('values',1)
+
+);
+
+
 });
